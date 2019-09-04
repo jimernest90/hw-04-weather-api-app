103644376742949e991a7a7939d20dd9b6b36c17
James White <jimernest90@yahoo.com>
2019-09-04 10:48:32 -0400

    trying to make ap request for city name

--- js/app.js
+++ js/app.js
@@ -1,2 +1,19 @@
-$(function () {
-})
+function search(cityName){
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q={city name}'
+    const apiKey = 'ad7c9971c4a75cf17e01bc59f26c5c8a'
+
+    $.ajax({
+      url: url,
+      type: "GET",
+      data: {q: city name, api_key: apiKey }
+    })
+    .done((response) => {
+      // execute this function if request is successful
+      console.log(response)
+      displayResults(response.data)
+    })
+    .fail(() => {
+      // execute this function if request fails
+      alert('error occurred')
+    })
+  }
