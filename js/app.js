d1d2d6a60db1ec986a9b3b5b610764737285a060
James White <jimernest90@yahoo.com>
2019-09-04 22:18:52 -0400

    added parameters

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
+/* #### ⚡️ Your page should have:
+- [ ] An **input field** for a user to enter a zip code or city name
+- [ ] A **submit button**
+- [ ] When the submit button is clicked:
+    - [ ] A **GET** request should fetch the weather data from the OpenWeather API
+    - [ ] The following data should be rendered to the page:
+        - [ ] City name
+        - [ ] Current temperature (displayed in Fahrenheit)
+        - [ ] Weather description
+        - [ ] Min temp
+        - [ ] Max temp
+- [ ] Have the temperature turn blue if under 40, and red if above 90.
+
+Here are some zip codes / city names to test!
+
+- 99501 (Anchorage)
+- 99723 (Barrow, AK)
+- 60605 (Chicago)
+- 70124 (New Orleans)
+- 77030 (Houston, TX)
+- 00902 (San Juan, Puerto Rico)
+- 46923 (Delphi, IN)
+- 94123 (San Francisco, CA) */
 function search(cityName){
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q={city name}'
+    const url = 'https://api.openweathermap.org'
     const apiKey = 'ad7c9971c4a75cf17e01bc59f26c5c8a'
 
     $.ajax({
       url: url,
       type: "GET",
-      data: {q: city name, api_key: apiKey }
+      data: {id: 99501, APPID: apiKey }
     })
     .done((response) => {
       // execute this function if request is successful
